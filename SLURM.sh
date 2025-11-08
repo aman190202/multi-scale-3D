b4e1c3d468da79e45d7d0de0d0c86cfe8a27f53d
Aman Agarwal <agarwalaman190202@gmail.com>
2025-11-08 18:44:22 -0500

    MapAnything inference added

--- SLURM.sh
+++ SLURM.sh
@@ -6,14 +6,13 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mem=24G
 #SBATCH --time=12:00:00
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=8
 
 # -----------------------------
 # Environment setup
 # -----------------------------
 
-SCRATCH=/users/aagar133/scratch/multi-scale-3D
-
+export SCRATCH=/users/aagar133/scratch/multi-scale-3D
 module load cuda/11.8.0-lpttyok
 module load cudnn/8.7.0.84-11.8-lg2dpd5
 module load miniconda3/23.11.0s
@@ -22,23 +21,27 @@ module load ffmpeg
 source /oscar/runtime/software/external/miniconda3/23.11.0/etc/profile.d/conda.sh
 
 # -----------------------------
-# Installation
+# Installation 
 # -----------------------------
+# Undo all the commented lines to create a first time setup
 
 # conda create --prefix $SCRATCH/mscale_env python=3.11 --solver classic -y
 conda activate $SCRATCH/mscale_env
+# conda install -c conda-forge colmap=3.11.0
 
-# pip install pandas protobuf packaging
+# pip install pandas protobuf packaging pillow
 # pip install torch==2.1.2+cu118 torchvision==0.16.2+cu118 --extra-index-url https://download.pytorch.org/whl/cu118
-# conda install -c "nvidia/label/cuda-11.8.0" cuda-toolkit
+# conda install -c "nvidia/label/cuda-11.8.0" cuda-toolkit --solver classic -y
+# pip install pillow
 # pip install ninja git+https://github.com/NVlabs/tiny-cuda-nn/#subdirectory=bindings/torch --no-build-isolation
 # pip install --user huggingface_hub
 # pip install nerfstudio
 
+
+
 # -----------------------------
 # DATASET INSTALLATION
 # -----------------------------
-
 # export HF_TOKEN=hf_yourtokenhere
 # python download.py
 
@@ -46,18 +49,16 @@ export SCRATCH=/users/aagar133/scratch/multi-scale-3D
 export DATASET=/users/aagar133/scratch/multi-scale-3D/satellite_to_street_dataset
 export SCENE=CIT-Brown-University
 
+
+# CUDA_VISIBLE_DEVICES=0 ns-process-data images --data $DATASET/$SCENE/footage --output-dir $DATASET/$SCENE/
+
+
 # -----------------------------
-# COLMAP SETUP 
+# MAP ANYTHING
 # -----------------------------
+# pip install "git+https://github.com/facebookresearch/map-anything.git@fde8425513178bb4f89fba9828193e6be3ece248#egg=map-anything[all]"
+
+# Using a stride 4 will only select every 4th image for map-anything inference ; we are doing this to not overload the memory 
+python map-anything-inference --scene_dir $DATASET/$SCENE-MapAnything --memory_efficient_inference -stride 4
 
 
-# singularity pull docker://colmap/colmap:latest
-# mkdir -p ~/bin
-# cat <<'EOF' > ~/bin/colmap
-# #!/bin/bash
-# singularity exec --nv $SCRATCH/multi-scale-3D/colmap_latest.sif colmap "$@"
-# EOF
-# chmod +x ~/bin/colmap
-# export PATH=$HOME/bin:$PATH
-
-ns-process-data images --data $DATASET/$SCENE/footage --output_dir $DATASET/$SCENE/nerfstudio --verbose
